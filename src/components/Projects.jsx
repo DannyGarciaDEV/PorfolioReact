8b0513d7a768fbc95d798a5447379c71e07f3b0f
Danny Garcia <dannygarciaDEV@gmail.com>
2025-02-03 19:09:12 -0500

    adding link to projects pages

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,22 +4,22 @@ export default function Projects() {
         <h2>Projects</h2>
         
         <div className="project">
-          <h3>My Not So Little Adventure</h3>
+          <h3> <a href="https://mynotsoadv.netlify.app/" target="_blank"> My Not So Little Adventure </a></h3>
           <p>It's not just about exploring; it's about having a blast and uncovering Beantown's coolest landmarks together!</p>
         </div>
         
         <div className="project">
-          <h3>Community Catalyst</h3>
+         <h3> <a href="https://community-catalyst.onrender.com/" target="_blank" >Community Catalyst</a> </h3>
           <p>With Community Catalyst, you can easily connect, organize, and make a meaningful impact.</p>
         </div>
         
         <div className="project">
-          <h3>Danny AI Python</h3>
+          <h3> <a href="https://github.com/DannyGarciaDEV/chatbotPython"target="_blank" > Danny AI Python </a></h3>
           <p>A simple Python chat bot that learns and responds based on user input. The bot uses a knowledge base stored in a JSON file.</p>
         </div>
         
         <div className="project">
-          <h3>Civics Test</h3>
+          <h3>  <a href="https://civicsreview.netlify.app/"target="_blank" > Civics Test </a></h3>
           <p>Learn the 100 questions for the civics test in English and Spanish!</p>
         </div>
       </div>
